5213dc3bf3e016613cc7b98a00aff8166ddc50a3
macariallison30-blip <macariallison30@gmail.com>
2026-03-24 15:23:56 -0400

    UserList updated
    Refactor UserList component to dynamically display users from API.

--- components/userList/userList.jsx
+++ components/userList/userList.jsx
@@ -3,46 +3,61 @@ import {
   Divider,
   List,
   ListItem,
-  ListItemText,
-  Typography,
-}
-from '@mui/material';
+  ListItemText
+} from '@mui/material';
+import { Link } from 'react-router-dom';
 import './userList.css';
 
-/**
- * Define UserList, a React component of project #5
- */
+import fetchModel from '../../lib/fetchModelData';
+
 class UserList extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    fetchModel('/user/list')
+      .then((response) => {
+        this.setState({
+          users: response.data
+        });
+      })
+      .catch((error) => {
+        console.error('Error loading users:', error);
+      });
   }
 
   render() {
     return (
-      <div>
-        <Typography variant="body1">
-          This is the user list, which takes up 3/12 of the window.
-          You might choose to use <a href="https://mui.com/components/lists/">Lists</a> and <a href="https://mui.com/components/dividers/">Dividers</a> to
-          display your users like so:
-        </Typography>
-        <List component="nav">
-          <ListItem>
-            <ListItemText primary="Item #1" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #2" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #3" />
-          </ListItem>
-          <Divider />
-        </List>
-        <Typography variant="body1">
-          The model comes in from window.models.userListModel()
-        </Typography>
-      </div>
+      <List component="nav">
+
+        {this.state.users.map((user) => (
+
+          <div key={user._id}>
+
+            <ListItem
+              button
+              component={Link}
+              to={`/users/${user._id}`}
+            >
+
+              <ListItemText
+                primary={`${user.first_name} ${user.last_name}`}
+              />
+
+            </ListItem>
+
+            <Divider />
+
+          </div>
+
+        ))}
+
+      </List>
     );
   }
 }
