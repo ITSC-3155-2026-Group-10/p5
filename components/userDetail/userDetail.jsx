ec1bf382a8b0e968cb82cbae4e78bb329a9b92af
macariallison30-blip <macariallison30@gmail.com>
2026-03-24 15:28:32 -0400

    UserDetail updated

--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -1,27 +1,99 @@
 import React from 'react';
 import {
-  Typography
+  Typography,
+  Button
 } from '@mui/material';
+import { Link } from 'react-router-dom';
 import './userDetail.css';
 
+import fetchModel from '../../lib/fetchModelData';
 
-/**
- * Define UserDetail, a React component of project #5
- */
 class UserDetail extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      user: null
+    };
+  }
+
+  componentDidMount() {
+    this.loadUser();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (
+      prevProps.match.params.userId !==
+      this.props.match.params.userId
+    ) {
+      this.loadUser();
+    }
+  }
+
+  loadUser() {
+
+    const userId = this.props.match.params.userId;
+
+    fetchModel(`/user/${userId}`)
+      .then((response) => {
+
+        const userData = response.data;
+
+        this.setState({
+          user: userData
+        });
+
+        if (this.props.changeContext) {
+          this.props.changeContext(
+            `${userData.first_name} ${userData.last_name}`
+          );
+        }
+
+      })
+      .catch((error) => {
+        console.error("Error loading user:", error);
+      });
   }
 
   render() {
+
+    if (!this.state.user) {
+      return <Typography>Loading...</Typography>;
+    }
+
+    const user = this.state.user;
+
     return (
-      <Typography variant="body1">
-        This should be the UserDetail view of the PhotoShare app. Since
-        it is invoked from React Router the params from the route will be
-        in property match. So this should show details of user:
-        {this.props.match.params.userId}. You can fetch the model for the
-        user from window.models.userModel(userId).
-      </Typography>
+
+      <div className="user-detail-container">
+
+        <Typography variant="h5">
+          {user.first_name} {user.last_name}
+        </Typography>
+
+        <Typography>
+          Location: {user.location}
+        </Typography>
+
+        <Typography>
+          Description: {user.description}
+        </Typography>
+
+        <Typography>
+          Occupation: {user.occupation}
+        </Typography>
+
+        <Button
+          variant="contained"
+          component={Link}
+          to={`/photos/${user._id}`}
+          sx={{ marginTop: "10px" }}
+        >
+          View Photos
+        </Button>
+
+      </div>
+
     );
   }
 }
