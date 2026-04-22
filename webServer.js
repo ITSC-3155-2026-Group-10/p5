c7716dc0388951a9f353ce7011bde8e465f8ca68
Macari Allison <macariallison30@gmail.com>
2026-04-22 13:42:44 -0400

    Backend Added

--- webServer.js
+++ webServer.js
@@ -1,35 +1,6 @@
-/**
- * This builds on the webServer of previous projects in that it exports the
- * current directory via webserver listing on a hard code (see portno below)
- * port. It also establishes a connection to the MongoDB named 'project6'.
- *
- * To start the webserver run the command:
- *    node webServer.js
- *
- * Note that anyone able to connect to localhost:portNo will be able to fetch
- * any file accessible to the current user in the current directory or any of
- * its children.
- *
- * This webServer exports the following URLs:
- * /            - Returns a text status message. Good for testing web server
- *                running.
- * /test        - Returns the SchemaInfo object of the database in JSON format.
- *                This is good for testing connectivity with MongoDB.
- * /test/info   - Same as /test.
- * /test/counts - Returns the population counts of the cs collections in the
- *                database. Format is a JSON object with properties being the
- *                collection name and the values being the counts.
- *
- * The following URLs need to be changed to fetch there reply values from the
- * database:
- * /user/list         - Returns an array containing all the User objects from
- *                      the database (JSON format).
- * /user/:id          - Returns the User object with the _id of id (JSON
- *                      format).
- * /photosOfUser/:id  - Returns an array with all the photos of the User (id).
- *                      Each photo should have all the Comments on the Photo
- *                      (JSON format).
- */
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const multer = require("multer");
 
 const mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
@@ -39,7 +10,15 @@ const async = require("async");
 const express = require("express");
 const app = express();
 
-// Load the Mongoose schema for User, Photo, and SchemaInfo
+// session + body parsing
+app.use(session({
+  secret: "secretKey",
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(bodyParser.json());
+
+// models
 const User = require("./schema/user.js");
 const Photo = require("./schema/photo.js");
 const SchemaInfo = require("./schema/schemaInfo.js");
@@ -50,65 +29,108 @@ mongoose.connect("mongodb://127.0.0.1/project6", {
   useUnifiedTopology: true,
 });
 
-// We have the express static module
-// (http://expressjs.com/en/starter/static-files.html) do all the work for us.
 app.use(express.static(__dirname));
 
 app.get("/", function (request, response) {
   response.send("Simple web server of files from " + __dirname);
 });
 
-/**
- * Use express to handle argument passing in the URL. This .get will cause
- * express to accept URLs with /test/<something> and return the something in
- * request.params.p1.
- * 
- * If implement the get as follows:
- * /test        - Returns the SchemaInfo object of the database in JSON format.
- *                This is good for testing connectivity with MongoDB.
- * /test/info   - Same as /test.
- * /test/counts - Returns an object with the counts of the different collections
- *                in JSON format.
- */
-app.get("/test/:p1", function (request, response) {
-  // Express parses the ":p1" from the URL and returns it in the request.params
-  // objects.
-  console.log("/test called with param1 = ", request.params.p1);
+// LOGIN
+app.post("/admin/login", function (request, response) {
+  const loginName = request.body.login_name;
+  const password = request.body.password;
 
+  if (!loginName || !password) {
+    response.status(400).send("Missing login_name or password");
+    return;
+  }
+
+  User.findOne({ login_name: loginName }, function (err, user) {
+    if (err) {
+      response.status(500).send(JSON.stringify(err));
+      return;
+    }
+
+    if (!user || user.password !== password) {
+      response.status(400).send("Invalid login credentials");
+      return;
+    }
+
+    // store logged-in user
+    request.session.user = {
+      _id: user._id,
+      first_name: user.first_name,
+      last_name: user.last_name
+    };
+
+    response.status(200).json({
+      _id: user._id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+    });
+  });
+});
+
+// LOGOUT
+app.post("/admin/logout", function (request, response) {
+  if (!request.session.user) {
+    response.status(400).send("No user currently logged in");
+    return;
+  }
+
+  request.session.destroy(function (err) {
+    if (err) {
+      response.status(500).send(JSON.stringify(err));
+      return;
+    }
+    response.status(200).send("Logged out");
+  });
+});
+
+// BLOCK all routes if not logged in
+app.use(function (request, response, next) {
+  // allow login, logout, AND test routes
+  if (
+    request.path.startsWith("/admin") ||
+    request.path.startsWith("/test") ||
+    request.path.startsWith("/user") ||
+    request.path.startsWith("/photosOfUser")
+  ) {
+    next();
+    return;
+  }
+
+  if (!request.session.user) {
+    response.status(401).send("Unauthorized");
+    return;
+  }
+
+  next();
+});
+
+// TEST ROUTES
+app.get("/test/:p1", function (request, response) {
   const param = request.params.p1 || "info";
 
   if (param === "info") {
-    // Fetch the SchemaInfo. There should only one of them. The query of {} will
-    // match it.
     SchemaInfo.find({}, function (err, info) {
       if (err) {
-        // Query returned an error. We pass it back to the browser with an
-        // Internal Service Error (500) error code.
-        console.error("Error in /user/info:", err);
         response.status(500).send(JSON.stringify(err));
         return;
       }
       if (info.length === 0) {
-        // Query didn't return an error but didn't find the SchemaInfo object -
-        // This is also an internal error return.
-        response.status(400).send("Missing SchemaInfo");
+        response.status(500).send("Missing SchemaInfo");
         return;
       }
-
-      // We got the object - return it in JSON format.
-      console.log("SchemaInfo", info[0]);
       response.end(JSON.stringify(info[0]));
     });
   } else if (param === "counts") {
-    // In order to return the counts of all the collections we need to do an
-    // async call to each collections. That is tricky to do so we use the async
-    // package do the work. We put the collections into array and use async.each
-    // to do each .count() query.
     const collections = [
       { name: "user", collection: User },
       { name: "photo", collection: Photo },
       { name: "schemaInfo", collection: SchemaInfo },
     ];
+
     async.each(
       collections,
       function (col, done_callback) {
@@ -130,85 +152,80 @@ app.get("/test/:p1", function (request, response) {
       }
     );
   } else {
-    // If we know understand the parameter we return a (Bad Parameter) (400)
-    // status.
     response.status(400).send("Bad param " + param);
   }
 });
 
-/**
- * URL /user/list - Returns all the User objects.
- */
+// USER LIST
 app.get("/user/list", function (request, response) {
-  User.find({}, {_id: 1, first_name: 1, last_name: 1}, function (err, users) {
+  User.find({}, { _id: 1, first_name: 1, last_name: 1 }, function (err, users) {
     if (err) {
-      // Query returned an error. We pass it back to the browser with an
-      // Internal Service Error (500) error code.
-      console.error("Error in /user/list", err);
       response.status(500).send(JSON.stringify(err));
       return;
     }
     if (users.length === 0) {
-      // Query didn't return an error but didn't find the SchemaInfo object -
-      // This is also an internal error return.
       response.status(400).send();
       return;
     }
-    // We got the object - return it in JSON format.
     response.end(JSON.stringify(users));
   });
 });
 
-/**
- * URL /user/:id - Returns the information for User (id).
- */
+// USER DETAIL
 app.get("/user/:id", function (request, response) {
   const id = request.params.id;
-  User.find({_id: {$eq: id}},{__v:0}, function (err, user) {
+
+  User.find({ _id: { $eq: id } }, { __v: 0 }, function (err, user) {
     if (err) {
-      // Query returned an error. We pass it back to the browser with an
-      // Internal Service Error (500) error code.
-      console.error("Error in /user/:id", err);
       response.status(500).send(JSON.stringify(err));
       return;
     }
     if (user.length === 0) {
-      // Query didn't return an error but didn't find the SchemaInfo object -
-      // This is also an internal error return.
       response.status(400).send();
       return;
     }
-    // We got the object - return it in JSON format.
     response.end(JSON.stringify(user[0]));
   });
 });
 
-/**
- * URL /photosOfUser/:id - Returns the Photos for User (id).
- */
+// PHOTOS OF USER
 app.get("/photosOfUser/:id", function (request, response) {
   const id = request.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    response.status(400).send("Invalid user id");
+    return;
+  }
+
   Photo.aggregate([
-    { $match:
-          {user_id: {$eq: new mongoose.Types.ObjectId(id)}}
+    {
+      $match: {
+        user_id: { $eq: new mongoose.Types.ObjectId(id) }
+      }
     },
-    { $addFields: {
-      comments: { $ifNull : ["$comments", []] }
-    } },
-    { $lookup: {
+    {
+      $addFields: {
+        comments: { $ifNull: ["$comments", []] }
+      }
+    },
+    {
+      $lookup: {
         from: "users",
         localField: "comments.user_id",
         foreignField: "_id",
         as: "users"
-      } },
-    { $addFields: {
+      }
+    },
+    {
+      $addFields: {
         comments: {
           $map: {
             input: "$comments",
             in: {
               $mergeObjects: [
                 "$$this",
-                { user: {
+                {
+                  user: {
                     $arrayElemAt: [
                       "$users",
                       {
@@ -218,13 +235,16 @@ app.get("/photosOfUser/:id", function (request, response) {
                         ]
                       }
                     ]
-                  } }
+                  }
+                }
               ]
             }
           }
         }
-      } },
-    { $project: {
+      }
+    },
+    {
+      $project: {
         users: 0,
         __v: 0,
         "comments.__v": 0,
@@ -233,32 +253,21 @@ app.get("/photosOfUser/:id", function (request, response) {
         "comments.user.description": 0,
         "comments.user.occupation": 0,
         "comments.user.__v": 0
-      } }
+      }
+    }
   ], function (err, photos) {
     if (err) {
-      // Query returned an error. We pass it back to the browser with an
-      // Internal Service Error (500) error code.
-      console.error("Error in /photosOfUser/:id", err);
       response.status(500).send(JSON.stringify(err));
       return;
     }
     if (photos.length === 0) {
-      // Query didn't return an error but didn't find the SchemaInfo object -
-      // This is also an internal error return.
       response.status(400).send();
       return;
     }
-    // We got the object - return it in JSON format.
     response.end(JSON.stringify(photos));
   });
 });
 
 const server = app.listen(3000, function () {
-  const port = server.address().port;
-  console.log(
-    "Listening at http://localhost:" +
-      port +
-      " exporting the directory " +
-      __dirname
-  );
+  console.log("Listening at http://localhost:3000");
 });
